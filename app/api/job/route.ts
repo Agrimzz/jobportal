2cc7a3e1918595f274918643badd9d29346d3003
Agrim Pradhananga <agrimp91@gmail.com>
2024-12-24 16:48:00 +0545

    fix build

--- app/api/job/route.ts
+++ app/api/job/route.ts
@@ -6,22 +6,19 @@ export const GET = async (req: Request) => {
   const { searchParams } = new URL(req.url)
   const limit = parseInt(searchParams.get("limit") || "6")
   const page = parseInt(searchParams.get("page") || "1")
-  const q = searchParams.get("q")
 
   try {
     await connectToDatabase()
 
     const skip = (page - 1) * limit
     await User.init()
-    const searchFilter = q ? { title: { $regex: q, $options: "i" } } : {}
-
-    const jobs = await Job.find(searchFilter)
+    const jobs = await Job.find()
       .populate("recruiter")
       .skip(skip)
       .limit(limit)
       .sort({ createdAt: -1 })
 
-    const totalJobs = await Job.countDocuments(searchFilter)
+    const totalJobs = await Job.countDocuments()
     const hasNextPage = skip + jobs.length < totalJobs
 
     return new Response(
